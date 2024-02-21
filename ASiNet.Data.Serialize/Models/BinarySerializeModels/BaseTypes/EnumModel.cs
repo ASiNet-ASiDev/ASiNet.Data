276ce6eec557aeee9614125db5187e9238da1cc2
Difoldery <87066000+DifolderyXXL@users.noreply.github.com>
2024-02-21 20:20:04 +0300

    Enum fix

--- ASiNet.Data.Serialize/Models/BinarySerializeModels/BaseTypes/EnumModel.cs
+++ ASiNet.Data.Serialize/Models/BinarySerializeModels/BaseTypes/EnumModel.cs
@@ -62,33 +62,85 @@ public class EnumModel<TEnum> : BaseSerializeModel<TEnum>
 
     public override object? DeserializeToObject(ISerializeReader reader)
     {
-        if (reader.CanReadSize(sizeof(bool)))
-        {
-            var buffer = (stackalloc byte[sizeof(bool)]);
-            reader.ReadBytes(buffer);
-            return BitConverter.ToBoolean(buffer);
-        }
+        return Deserialize(reader);
         throw new Exception();
     }
 
     public override void Serialize(TEnum obj, ISerializeWriter writer)
     {
-        var buffer = (stackalloc byte[sizeof(bool)]);
+        var etype = typeof(TEnum).GetEnumUnderlyingType();
         
+        if (etype == typeof(byte))
+        {
+            BinarySerializer.SharedSerializeContext
+                .GetOrGenerate<byte>()
+                .Serialize((byte)(object)obj, writer);
+            return;
+        }
+
+        if (etype == typeof(int))
+        {
+            BinarySerializer.SharedSerializeContext
+                .GetOrGenerate<int>()
+                .Serialize((int)(object)obj, writer);
+            return;
+        }
+        
+        if (etype == typeof(short))
+        {
+            BinarySerializer.SharedSerializeContext
+                .GetOrGenerate<short>()
+                .Serialize((short)(object)obj, writer);
+            return;
+        }
+
+        if (etype == typeof(long))
+        {
+            BinarySerializer.SharedSerializeContext
+                .GetOrGenerate<long>()
+                .Serialize((long)(object)obj, writer);
+            return;
+        }
+
+        if (etype == typeof(sbyte))
+        {
+            BinarySerializer.SharedSerializeContext
+                .GetOrGenerate<byte>()
+                .Serialize((byte)(object)obj, writer);
+            return;
+        }
+
+        if (etype == typeof(uint))
+        {
+            BinarySerializer.SharedSerializeContext
+                .GetOrGenerate<uint>()
+                .Serialize((uint)(object)obj, writer);
+            return;
+        }
+        
+        if (etype == typeof(ushort))
+        {
+            BinarySerializer.SharedSerializeContext
+                .GetOrGenerate<ushort>()
+                .Serialize((ushort)(object)obj, writer);
+            return;
+        }
+
+        if (etype == typeof(ulong))
+        {
+            BinarySerializer.SharedSerializeContext
+                .GetOrGenerate<ulong>()
+                .Serialize((ulong)(object)obj, writer);
+            return;
+        }
         throw new Exception();
     }
 
     public override void SerializeObject(object? obj, ISerializeWriter writer)
     {
-        if (obj is bool value)
+        if (obj is TEnum value)
         {
-            var buffer = (stackalloc byte[sizeof(bool)]);
-            if (value.TryToBytes(buffer))
-            {
-                writer.WriteBytes(buffer);
-                return;
-            }
-            throw new Exception();
+            Serialize(value, writer);
         }
         throw new Exception();
     }
