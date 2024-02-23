5d9e786edcfc17a4963a3d1144fb185280728b49
Difoldery <87066000+DifolderyXXL@users.noreply.github.com>
2024-02-23 17:05:29 +0300

    NullableTypes

--- ASiNet.Data.Serialize/Models/NullableTypesModel.cs
+++ ASiNet.Data.Serialize/Models/NullableTypesModel.cs
@@ -1,36 +1,73 @@
 ﻿using System.Data.SqlTypes;
+using System.Linq.Expressions;
+using System.Reflection;
 using ASiNet.Data.Serialization.Interfaces;
 
 namespace ASiNet.Data.Serialization.Models;
-public class NullableTypesModel<T> : BaseSerializeModel<T>
+public class NullableTypesModel<T> : BaseSerializeModel<T?>
 {
-
     private Lazy<Type> _underlyingType = new(() => Nullable.GetUnderlyingType(typeof(T))!);
 
     private Lazy<ISerializeModel> _underlyingSerializeModel = 
         new(() => BinarySerializer.SharedSerializeContext.GetOrGenerate(Nullable.GetUnderlyingType(typeof(T))!));
+    
+    private Lazy<Func<T, object>> NullableGetValueLambda = new(() =>
+    {
+        var instParam = Expression.Parameter(typeof(T));
 
+        var resultProp = Expression.Convert(Expression.Property(instParam, nameof(Nullable<byte>.Value)), typeof(object));
+
+        return Expression.Lambda<Func<T, object>>(resultProp, instParam).Compile();
+    });
+    
+    private Lazy<Func<object, T>> CreateNullableLambda = new(() =>
+    {
+        var instParam = Expression.Parameter(typeof(object));
+
+        var type = Nullable.GetUnderlyingType(typeof(T))!;
+        
+        var body = Expression.New(
+            typeof(T).GetConstructor(BindingFlags.Public|BindingFlags.Instance, new []{type}), 
+            Expression.Convert(instParam, type));    
+        
+        return Expression.Lambda<Func<object, T>>(body, instParam).Compile();
+    });
+    
+    
     public override void Serialize(T obj, ISerializeWriter writer)
     {
-        if(obj is null)
+        byte val = (byte)(obj is null ? 0 : 1);
+        writer.WriteByte(val);
+        
+        if (val == 1)
         {
-
+            var instance = NullableGetValueLambda.Value(obj);
+            _underlyingSerializeModel.Value.SerializeObject(instance, writer);
         }
-        throw new NotImplementedException();
     }
 
     public override void SerializeObject(object? obj, ISerializeWriter writer)
     {
-        throw new NotImplementedException();
+        Serialize((T)obj, writer);
     }
 
     public override T? Deserialize(ISerializeReader reader)
     {
-        throw new NotImplementedException();
+        byte val = reader.ReadByte();
+        if (val == 0)
+            return default;
+        
+        if (val == 1)
+        {
+            var instance = _underlyingSerializeModel.Value.DeserializeToObject(reader);
+            return CreateNullableLambda.Value(instance);
+        }
+
+        throw new Exception();
     }
 
     public override object? DeserializeToObject(ISerializeReader reader)
     {
-        throw new NotImplementedException();
+        return Deserialize(reader);
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,15 +1,20 @@
-﻿using ASiNet.Data.Base.Serialization.Models;
-using ASiNet.Data.Serialization;
-using ASiNet.Data.Serialization.Base.Models;
+﻿using ASiNet.Data.Serialization;
 using ASiNet.Data.Serialization.IO.Arrays;
+using ASiNet.Data.Serialization.Models;
 using ASiNet.Data.Serialization.Models.BinarySerializeModels;
 using ASiNet.Data.Serialization.Models.BinarySerializeModels.BaseTypes;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
-Console.ReadLine();
 
-enum Test : int
-{
-    a1 = 2,
-}
+var buf = new byte[64];
+
+int? val = null;
+
+var model = new NullableTypesModel<int?>();
+
+model.Serialize(val, (ArrayWriter)buf);
+
+var res = model.Deserialize((ArrayReader)buf);
+
+Console.ReadLine();
