7d99f157dd0dee573a9433e4155ffcef6b425db6
Александр <87065876+ASiNet-ASiDev@users.noreply.github.com>
2024-03-09 13:40:21 +0300

    rg

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System.Diagnostics.Metrics;
 using System.Text;
 using ASiNet.Data.Serialization;
+using ASiNet.Data.Serialization.Attributes;
 using ASiNet.Data.Serialization.Interfaces;
 using ASiNet.Data.Serialization.Models;
-using Test;
-
 
 
 Console.ReadLine();
